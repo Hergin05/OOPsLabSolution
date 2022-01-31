f8a91e339bd2210d5f1554acd61efac5bd9e8094
Hergin05 <61073545+Hergin05@users.noreply.github.com>
2022-01-31 11:33:21 +0530

    Add files via upload

---
+++ Lab1/src/admin/CredentialService.java
@@ -0,0 +1,33 @@
+package admin;
+
+public class CredentialService {
+	
+	public char[] generatePassword() {
+
+		String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String smallLetters = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String specialCharacters = "!@#$%^&*_=+-/.?<>)";
+
+		String values = capitalLetters + smallLetters + numbers + specialCharacters;
+		
+		char[] password = new char[8];
+
+		for (int i = 0; i < 8; i++) {
+			password[i] = values.charAt((int) (Math.random()*values.length()));
+		}
+		return password;
+	}
+	
+	public String generateEmailAddress(String firstName, String lastName, String department)
+	{
+		return firstName+lastName+"@"+department+".abc.com";
+	}
+	
+	public void showCredentials(Employee employee, String email, char[] generatedPassword) {
+		System.out.println("Dear "+employee.getFirstName()+ " your generated credentials are as follows ");
+		System.out.println("Email --> "+ email);
+		System.out.print("Password --> 1"+ "");
+		System.out.print(generatedPassword);
+	}
+}
